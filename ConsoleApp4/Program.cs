24bdce0123ebd690434fdb8dbc41a5cbd003bbba
Masha <Masha@ZENBOOK-84>
2022-09-20 22:15:56 -0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = new int[25];
+            sequence[0] = 0;
+            sequence[1] = 1;
+            for (int counter = 2; counter <= 24; counter++)
+                sequence[counter] = sequence[counter - 2] + sequence[counter - 1];
+            for (int counter = 0; counter <= 24; counter++)
+                Console.WriteLine("Fibonacci Number " + counter + " is " + sequence[counter]);
+        }
+    }
+}
